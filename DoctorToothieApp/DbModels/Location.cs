5bc505b0502f01fab5a6e67f1f3536ad1691c38d
ProSeven-hub <patryk20006@op.pl>
2024-12-17 21:54:56 +0100

    Dodanie pola Image do bazy lokalizacji

--- DoctorToothieApp/DbModels/Location.cs
+++ DoctorToothieApp/DbModels/Location.cs
@@ -5,6 +5,7 @@ public class Location {
     public int Id { get; set; }
     public string Name { get; set; } = default!;
     public string Address { get; set; } = default!;
+    public string Image { get; set; } = default!;
     public IList<Room> Rooms { get; set; } = [];
     public IList<User> Employees { get; set; } = [];
 
